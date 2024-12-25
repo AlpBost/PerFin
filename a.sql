ab21a95e085110eceee49db9592571babf3e566d
AlpBost <alpbost1@gmail.com>
2024-12-25 18:20:35 +0300

    Last Version Of Perfin

---
+++ a.sql
@@ -0,0 +1,81 @@
+
+
+
+SELECT name FROM sqlite_master WHERE type='table' AND name NOT LIKE 'sqlite_%';
+
+CREATE TABLE 
+deneme (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    age INTEGER,
+    email TEXT UNIQUE
+)
+
+drop table deneme
+
+CREATE TABLE
+ doas ( 
+    date TEXT UNIQUE,
+    close_price REAL
+    )
+
+drop table doas
+
+create TABLE
+    symbols (
+        symbol TEXT UNIQUE
+        )
+
+insert into symbols (symbol) values ("doas");
+insert into symbols (symbol) values ("garan");
+insert into symbols (symbol) values ("isctr");
+insert into symbols (symbol) values ("kchol");
+insert into symbols (symbol) values ("pgsus");
+insert into symbols (symbol) values ("sahol");
+insert into symbols (symbol) values ("thyao");    
+
+select * from doas
+select * from isctr
+select * from thyao
+select * from person
+select * from symbols
+select * from last_operations
+select * from portfolio
+select * from bist100
+
+
+insert into doas (date, close_price) values("aaa", 32.5)
+DELETE from doas where date = "aaa" and close_price = 32.5
+
+
+create table 
+    portfolio (
+        symbol TEXT UNIQUE,
+        amount INTEGER,
+        avgPrice REAL
+        )
+
+create table
+    last_operations (
+        symbol TEXT,
+        date TEXT,
+        amount INTEGER,
+        price REAL,
+        side TEXT
+        )
+
+drop table last_operations
+drop table portfolio
+
+insert into last_operations (symbol, date, amount, price, side) values ("doas" , "01/01/2024" , 5 , 31.31 ,"BUY")
+insert into portfolio (symbol, amount, avgPrice) values ("doas", 5 , 101.32)
+
+
+select exists(select 1 from portfolio where symbol = "doas")
+
+delete from last_operations where symbol = "doas"
+
+create table bist100 ( date TEXT UNIQUE, close_price REAL)
+
+
+delete FROM last_operations where date = "2024-12-02 Hour: 00:10"
